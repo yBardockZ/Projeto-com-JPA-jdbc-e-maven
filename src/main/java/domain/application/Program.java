b83a35b4fc6dbe5b08d1864080b9990d54f81dc6
Thalles <thalles_leopoldino@outlook.com>
2024-04-02 05:58:59 -0300

    Menu e opção 2 criados.

--- src/main/java/domain/application/Program.java
+++ src/main/java/domain/application/Program.java
@@ -1,7 +1,10 @@
 package domain.application;
 
 import java.time.LocalDate;
-import java.util.Arrays;
+import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,43 +16,130 @@ import domain.entities.Worker;
 import domain.enums.WorkerLevel;
 
 public class Program {
-	
+
 	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUN");
 	private static EntityManager em = emf.createEntityManager();
+	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
-		em.getTransaction().begin();
-		
-		Department department = new Department("Design");
-		
-		em.persist(department);
-		
-		HourContract h1 = new HourContract(LocalDate.of(2018, 8, 20), 50.00, 20);
-		HourContract h2 = new HourContract(LocalDate.of(2018, 6, 13), 30.00, 18);
-		HourContract h3 = new HourContract(LocalDate.of(2018, 8, 25), 80.00, 10);
-		
-		Worker worker = new Worker();
-		worker.setName("Alex");
-		worker.setLevel(WorkerLevel.MID_LEVEL);
-		worker.setBaseSalary(1200.00);
-		
-		h1.setWorker(worker);
-		h2.setWorker(worker);
-		h3.setWorker(worker);
-		em.persist(h1);
-		em.persist(h2);
-		em.persist(h3);
-		
-		worker.setContracts(Arrays.asList(h1, h2, h3));
-		worker.setDepartment(department);
-		
-		em.persist(worker);
-		
-		em.getTransaction().commit();
-		
+		menu();
+
+	}
+
+	public static void menu() {
+
+		int opcao;
+
+		do {
+
+			System.out.println("\nMenu:");
+			System.out.println("1. Check list of workers");
+			System.out.println("2. Register a worker");
+			System.out.println("3. Check income");
+			System.out.println("4. Exit");
+
+			System.out.print("Choose an option: ");
+			opcao = sc.nextInt();
+
+			switch (opcao) {
+			case 1:
+					System.out.println("show");
+					
+				break;
+
+			case 2:
+
+				try {
+
+					DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+					em.getTransaction().begin();
+
+					Worker worker = new Worker();
+
+					System.out.print("\nEnter department's name: ");
+					sc.nextLine();
+					String departmentName = sc.nextLine();
+
+					Department department = new Department(departmentName);
+					em.persist(department);
+
+					worker.setDepartment(department);
+
+					System.out.println("Enter worker data: ");
+
+					System.out.print("Name: ");
+					String workerName = sc.nextLine();
+					worker.setName(workerName);
+
+					System.out.print("Level: ");
+					String levelString = sc.nextLine();
+					worker.setLevel(WorkerLevel.valueOf(levelString));
+
+					System.out.print("Base salary: ");
+					Double baseSalary = sc.nextDouble();
+					worker.setBaseSalary(baseSalary);
+
+					System.out.print("How many contracts to this worker? ");
+					sc.nextLine();
+					Integer num = sc.nextInt();
+
+					if (num > 0) {
+						for (int i = 0; i < num; i++) {
+							System.out.println("\nEnter contract #" + (i + 1) + " data:");
+
+							System.out.print("Date (dd/mm/yyyy): ");
+							sc.nextLine();
+							String dateString = sc.nextLine();
+
+							System.out.print("Value per hour: ");
+							double valuePerHour = sc.nextDouble();
+
+							System.out.print("Duration (hours): ");
+							int duration = sc.nextInt();
+
+							LocalDate date = LocalDate.parse(dateString, fmt);
+							HourContract contract = new HourContract(date, valuePerHour, duration);
+							contract.setWorker(worker);
+
+							em.persist(contract);
+							worker.addContract(contract);
+						}
+					}
+
+					em.persist(worker);
+					em.getTransaction().commit();
+
+					System.out.println("Successfully registered");
+
+				} catch (InputMismatchException e) {
+					System.out.println(e.getMessage());
+					e.printStackTrace();
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					e.printStackTrace();
+				}
+
+				break;
+
+			case 3:
+				System.out.println("Você escolheu a opção 3.");
+
+				break;
+
+			case 4:
+				System.out.println("Saindo...");
+				break;
+
+			default:
+				System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+			}
+
+		} while (opcao != 4);
+
+		sc.close();
 		em.close();
-		
+
 	}
 
 }
